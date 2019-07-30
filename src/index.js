3537ac70cfbae34a1d94f5cd371fe03c0368bf56
Ross Libby <ross@rosslibby.com>
2019-07-30 16:54:32 -0500

    added the main js file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+  <div>
+    <h1>Counter</h1>
+  </div>
+)
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
