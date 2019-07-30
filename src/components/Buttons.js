b388379bd7dd7fedb7ec9348f219c1b21e1f3a64
Ross Libby <ross@rosslibby.com>
2019-07-30 17:23:27 -0500

    built buttons component

---
+++ src/components/Buttons.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { decrementCounter, incrementCounter } from '../actions/index'
+
+const mapDispatchToProps = dispatch => {
+  return {
+    incrementCounter: count => dispatch(incrementCounter(count)),
+    decrementCounter: count => dispatch(decrementCounter(count))
+  }
+}
+
+class ConnectedButtons extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      count: 0
+    }
+
+    this._increment = this._increment.bind(this)
+    this._decrement = this._decrement.bind(this)
+  }
+
+  _increment() {
+    let { count } = this.state
+    count++
+    this.props._increment(count)
+    this.setState({ count })
+  }
+
+  _decrement() {
+    let { count } = this.state
+    count--
+    this.props._decrement(count)
+    this.setState({ count })
+  }
+}
+
+render() {
+  return (
+    <div>
+      <button onClick={this._decrement}>-</button>
+      <button onClick={this._increment}>+</button>
+    </div>
+  )
+}
+
+const Buttons = connect(null, mapDispatchToProps)(ConnectedButtons)
+export default Buttons
