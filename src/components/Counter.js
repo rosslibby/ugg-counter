404442843575c2ba9628618626f2fd37cfad490e
Ross Libby <ross@rosslibby.com>
2019-07-30 17:04:43 -0500

    added store

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 
 export default class Counter extends Component {
   render () {
-    return <div />
+    return (
+      <div>
+        <h2></h2>
+      </div>
+    )
   }
 }
