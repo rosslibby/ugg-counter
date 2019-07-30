eac9833572a7f602e8c64221b2a747d4431be8a8
Ross Libby <ross@rosslibby.com>
2019-07-30 17:09:40 -0500

    added reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,20 @@
+import { DECREMENT_COUNTER, INCREMENT_COUNTER } from '../constants/action-types'
+
+const initialState = {
+  count: 0
+}
+
+export default const rootReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case DECREMENT_COUNTER:
+      return Object.assign({}, state, {
+        count: action.count
+      })
+    case INCREMENT_COUNTER:
+      return Object.assign({}, state, {
+        count: action.count
+      })
+    default:
+      return state
+  }
+}
